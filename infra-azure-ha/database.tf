a57cdcffad059f87aa2b9cb294c434fb05be0ded
Moncef <mben-soula@sonatype.com>
2025-09-26 12:46:47 -0700

    CLM-36249 - Configure Azure Container Instances for HA

---
+++ infra-azure-ha/database.tf
@@ -0,0 +1,133 @@
+# Private DNS Zone for PostgreSQL (required for private endpoints)
+resource "azurerm_private_dns_zone" "postgres" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.iq_rg.name
+
+  tags = merge(var.common_tags, {
+    Name = "pdns-postgres-ha"
+  })
+}
+
+# Link the private DNS zone to the virtual network
+resource "azurerm_private_dns_zone_virtual_network_link" "postgres" {
+  name                  = "pdns-link-postgres-ha"
+  resource_group_name   = azurerm_resource_group.iq_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.postgres.name
+  virtual_network_id    = azurerm_virtual_network.iq_vnet.id
+  registration_enabled  = false
+
+  tags = merge(var.common_tags, {
+    Name = "pdns-link-postgres-ha"
+  })
+}
+
+# PostgreSQL Flexible Server with Zone Redundancy (HA)
+resource "azurerm_postgresql_flexible_server" "iq_db_ha" {
+  name                   = "psqlfs-ref-arch-iq-ha"
+  resource_group_name    = azurerm_resource_group.iq_rg.name
+  location               = azurerm_resource_group.iq_rg.location
+  version                = var.postgres_version
+  delegated_subnet_id    = azurerm_subnet.db_subnet.id
+  private_dns_zone_id    = azurerm_private_dns_zone.postgres.id
+  administrator_login    = var.db_username
+  administrator_password = var.db_password
+
+  # Disable public network access when using VNet integration
+  public_network_access_enabled = false
+
+  # Zone redundancy for HA (equivalent to AWS Aurora Multi-AZ)
+  zone = "1"
+  high_availability {
+    mode                      = var.db_high_availability_mode # ZoneRedundant
+    standby_availability_zone = "2"
+  }
+
+  # Storage configuration for HA
+  storage_mb   = 65536 # 64GB initial
+  storage_tier = "P6"  # Premium tier for better performance (P6 is minimum for 64GB)
+
+  # SKU for HA workload (equivalent to AWS db.r6g.large)
+  sku_name = var.db_sku_name # GP_Standard_D4s_v3 = 4 vCores, 16GB RAM
+
+  # Backup configuration for HA
+  backup_retention_days        = var.db_backup_retention_days
+  geo_redundant_backup_enabled = var.db_geo_redundant_backup_enabled
+
+  # Security
+  create_mode = "Default"
+
+  # Maintenance window
+  maintenance_window {
+    day_of_week  = 0 # Sunday
+    start_hour   = 4
+    start_minute = 0
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "psqlfs-ref-arch-iq-ha"
+  })
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.postgres]
+}
+
+# Database for Nexus IQ Server
+resource "azurerm_postgresql_flexible_server_database" "iq_database_ha" {
+  name      = var.db_name
+  server_id = azurerm_postgresql_flexible_server.iq_db_ha.id
+  collation = "en_US.utf8"
+  charset   = "utf8"
+}
+
+# PostgreSQL Configuration for IQ Server optimization (similar to AWS Aurora parameter groups)
+resource "azurerm_postgresql_flexible_server_configuration" "shared_preload_libraries" {
+  name      = "shared_preload_libraries"
+  server_id = azurerm_postgresql_flexible_server.iq_db_ha.id
+  value     = "pg_stat_statements"
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "log_statement" {
+  name      = "log_statement"
+  server_id = azurerm_postgresql_flexible_server.iq_db_ha.id
+  value     = "all"
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "log_min_duration_statement" {
+  name      = "log_min_duration_statement"
+  server_id = azurerm_postgresql_flexible_server.iq_db_ha.id
+  value     = "1000" # Log queries taking longer than 1 second
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "log_checkpoints" {
+  name      = "log_checkpoints"
+  server_id = azurerm_postgresql_flexible_server.iq_db_ha.id
+  value     = "on"
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "log_connections" {
+  name      = "log_connections"
+  server_id = azurerm_postgresql_flexible_server.iq_db_ha.id
+  value     = "on"
+}
+
+resource "azurerm_postgresql_flexible_server_configuration" "log_disconnections" {
+  name      = "log_disconnections"
+  server_id = azurerm_postgresql_flexible_server.iq_db_ha.id
+  value     = "on"
+}
+
+# Firewall rule to allow Container Apps private subnets
+resource "azurerm_postgresql_flexible_server_firewall_rule" "container_apps" {
+  count            = length(var.private_subnet_cidrs)
+  name             = "AllowContainerApps-${count.index + 1}"
+  server_id        = azurerm_postgresql_flexible_server.iq_db_ha.id
+  start_ip_address = cidrhost(var.private_subnet_cidrs[count.index], 1)
+  end_ip_address   = cidrhost(var.private_subnet_cidrs[count.index], -2)
+}
+
+# Additional firewall rule for Azure services
+resource "azurerm_postgresql_flexible_server_firewall_rule" "azure_services" {
+  name             = "AllowAzureServices"
+  server_id        = azurerm_postgresql_flexible_server.iq_db_ha.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
---
+++ infra-azure-ha/outputs.tf
@@ -0,0 +1,153 @@
+# Application Gateway URL (Primary access point for HA deployment)
+output "application_gateway_url" {
+  description = "Application Gateway URL for accessing Nexus IQ Server HA cluster"
+  value       = "http://${azurerm_public_ip.app_gw_pip_ha.fqdn != null ? azurerm_public_ip.app_gw_pip_ha.fqdn : azurerm_public_ip.app_gw_pip_ha.ip_address}"
+}
+
+output "application_gateway_fqdn" {
+  description = "Application Gateway public IP FQDN"
+  value       = azurerm_public_ip.app_gw_pip_ha.fqdn
+}
+
+# Container App URL (Direct access to HA cluster)
+output "container_app_url" {
+  description = "Container App direct URL for HA cluster"
+  value       = "https://${azurerm_container_app.iq_app_ha.ingress[0].fqdn}"
+}
+
+output "container_app_fqdn" {
+  description = "Container App FQDN"
+  value       = azurerm_container_app.iq_app_ha.ingress[0].fqdn
+}
+
+# HA Configuration Details
+output "ha_configuration" {
+  description = "High Availability configuration details"
+  value = {
+    min_replicas       = var.iq_min_replicas
+    max_replicas       = var.iq_max_replicas
+    availability_zones = var.app_gateway_zones
+    database_ha_mode   = var.db_high_availability_mode
+    storage_redundancy = var.storage_account_replication_type
+  }
+}
+
+# Database Information
+output "database_endpoint" {
+  description = "PostgreSQL Flexible Server endpoint (Zone-redundant)"
+  value       = azurerm_postgresql_flexible_server.iq_db_ha.fqdn
+  sensitive   = false
+}
+
+output "database_name" {
+  description = "Database name for Nexus IQ Server"
+  value       = azurerm_postgresql_flexible_server_database.iq_database_ha.name
+}
+
+output "database_ha_status" {
+  description = "Database High Availability configuration"
+  value = {
+    ha_mode              = azurerm_postgresql_flexible_server.iq_db_ha.high_availability[0].mode
+    primary_zone         = azurerm_postgresql_flexible_server.iq_db_ha.zone
+    standby_zone         = azurerm_postgresql_flexible_server.iq_db_ha.high_availability[0].standby_availability_zone
+    geo_redundant_backup = azurerm_postgresql_flexible_server.iq_db_ha.geo_redundant_backup_enabled
+  }
+}
+
+# Storage Information
+output "storage_account_name" {
+  description = "Storage account name for HA file share"
+  value       = azurerm_storage_account.iq_storage_ha.name
+}
+
+output "file_share_name" {
+  description = "Azure Files share name for clustering"
+  value       = azurerm_storage_share.iq_data_ha.name
+}
+
+output "storage_redundancy" {
+  description = "Storage redundancy configuration"
+  value       = azurerm_storage_account.iq_storage_ha.account_replication_type
+}
+
+# Key Vault Information
+output "key_vault_uri" {
+  description = "Key Vault URI for secrets management"
+  value       = azurerm_key_vault.iq_kv_ha.vault_uri
+  sensitive   = false
+}
+
+# Container App Environment
+output "container_app_environment_id" {
+  description = "Container App Environment ID"
+  value       = azurerm_container_app_environment.iq_env_ha.id
+}
+
+output "container_app_environment_default_domain" {
+  description = "Container App Environment default domain"
+  value       = azurerm_container_app_environment.iq_env_ha.default_domain
+}
+
+# Log Analytics
+output "log_analytics_workspace_id" {
+  description = "Log Analytics Workspace ID for monitoring"
+  value       = azurerm_log_analytics_workspace.iq_logs_ha.id
+}
+
+# Application Insights (if enabled)
+output "application_insights_instrumentation_key" {
+  description = "Application Insights instrumentation key"
+  value       = var.enable_monitoring ? azurerm_application_insights.iq_insights_ha[0].instrumentation_key : null
+  sensitive   = true
+}
+
+output "application_insights_connection_string" {
+  description = "Application Insights connection string"
+  value       = var.enable_monitoring ? azurerm_application_insights.iq_insights_ha[0].connection_string : null
+  sensitive   = true
+}
+
+# Network Information
+output "virtual_network_id" {
+  description = "Virtual Network ID"
+  value       = azurerm_virtual_network.iq_vnet.id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs for Container Apps (Multi-zone)"
+  value       = azurerm_subnet.private_subnets[*].id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs for Application Gateway (Multi-zone)"
+  value       = azurerm_subnet.public_subnets[*].id
+}
+
+# Resource Group
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = azurerm_resource_group.iq_rg.name
+}
+
+output "resource_group_location" {
+  description = "Resource group location"
+  value       = azurerm_resource_group.iq_rg.location
+}
+
+# HA Deployment Summary
+output "ha_deployment_summary" {
+  description = "High Availability deployment summary"
+  value = {
+    deployment_type      = "high-availability"
+    container_replicas   = "${var.iq_min_replicas}-${var.iq_max_replicas}"
+    database_type        = "PostgreSQL Flexible Server"
+    database_ha          = var.db_high_availability_mode
+    storage_type         = "Azure Files Premium"
+    storage_redundancy   = var.storage_account_replication_type
+    load_balancer_type   = "Application Gateway v2"
+    availability_zones   = join(", ", var.app_gateway_zones)
+    auto_scaling_enabled = "Yes"
+    monitoring_enabled   = var.enable_monitoring ? "Yes" : "No"
+    backup_enabled       = var.enable_backup ? "Yes" : "No"
+  }
+}
